7b10c89bf688a947fa73a07d2df44f6f410f9094
jtareen <jtareen440@gmail.com>
2024-10-11 21:13:29 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lab4_task3
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/lab4_task3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab4_task3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
